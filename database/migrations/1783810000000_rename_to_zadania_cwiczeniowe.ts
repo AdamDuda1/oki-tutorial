cf615401175d7a50d121b92ac7f404f51d5df93b
Adam Duda <adam.duda@post.com>
2026-07-02 11:25:08 +0200

    quick fix

--- database/migrations/1783810000000_rename_to_zadania_cwiczeniowe.ts
+++ database/migrations/1783810000000_rename_to_zadania_cwiczeniowe.ts
@@ -4,8 +4,6 @@ export default class extends BaseSchema {
   async up() {
     this.schema.alterTable('tematy', (table) => {
       table.renameColumn('zadania_rozgrzewkowe', 'zadania_cwiczeniowe')
-      table.json('zadania_na_pomysl').nullable()
-      table.json('zadania_treningowe').nullable()
     })
   }
 
